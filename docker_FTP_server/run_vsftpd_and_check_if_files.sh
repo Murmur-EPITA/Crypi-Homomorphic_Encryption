8dc8b02afbfab949390bb5f7ee4961964fcb5a52
Angel Bochenko <angel.bochenko@epita.fr>
2023-05-08 21:30:05 +0200

    ADD: docker ftp server: script to search for context.bin and enc_data.b64 that run every 10 seconds

---
+++ docker_FTP_server/run_vsftpd_and_check_if_files.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+CONTEXT="/data/context.bin"
+ENC_DATA="/data/enc_data.b64"
+
+vsftpd /etc/vsftpd/vsftpd2.conf &
+
+while true
+do
+    if [ -f "$CONTEXT" ] && [ -f "$ENC_DATA" ]
+    then
+        echo "STARTING PROCESS..."
+    fi
+    sleep 10
+done
+
