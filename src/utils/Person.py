22b78a7a5297298d62fefba6ffe4948a79e3de12
Angel Bochenko <angel.bochenko@epita.fr>
2023-05-08 15:12:10 +0200

    MODIFY: Person class: add statistics + refactoring

--- src/utils/Person.py
+++ src/utils/Person.py
@@ -1,6 +1,7 @@
 from statistics import mean, stdev, median
 from typing import Dict
 import pandas as pd
+from json import dumps
 
 class Person:
     def __init__(self, male, age, education, currentSmoker, cigsPerDay,
@@ -27,89 +28,48 @@ class PersonList:
     def __init__(self, persons=[]):
         self.persons = persons
         if self.persons != []:
-            self.persons = self.compute_stats()
+            self.stats = dumps(self.compute_stats(), indent=4)
         else:
             self.stats = {}
 
-    # Dict[nom_colonne, Dict[moyenne, ecart-type, mediane]]
-    def compute_stats(self) -> Dict[str, Dict[float, float, float]]:
-        maleList = [person.male for person in self.persons]
-        ageList = [person.age for person in self.persons]
-        educationList = [person.education for person in self.persons]
-        currentSmokerList = [person.currentSmoker for person in self.persons]
-        cigsPerDayList = [person.cigsPerDay for person in self.persons]
-        BPMedsList = [person.BPMeds for person in self.persons]
-        prevalentStrokeList = [person.prevalentStroke for person in self.persons]
-        prevalentHypList = [person.prevalentHyp for person in self.persons]
-        diabetesList = [person.diabetes for person in self.persons]
-        totCholList = [person.totChol for person in self.persons]
-        sysBPList = [person.sysBP for person in self.persons]
-        diaBPList = [person.diaBP for person in self.persons]
-        BMIList = [person.BMI for person in self.persons]
-        heartRateList = [person.heartRate for person in self.persons]
-        glucoseList = [person.glucose for person in self.persons]
-        tenYearCHDList = [person.TenYearCHD for person in self.persons]
+    def compute_stats(self) -> Dict[str, Dict[str, float]]:
+        data = {
+            'male': [person.male for person in self.persons],
+            'age': [person.age for person in self.persons],
+            'education': [person.education for person in self.persons],
+            'currentSmoker': [person.currentSmoker for person in self.persons],
+            'cigsPerDay': [person.cigsPerDay for person in self.persons],
+            'BPMeds': [person.BPMeds for person in self.persons],
+            'prevalentStroke': [person.prevalentStroke for person in self.persons],
+            'prevalentHyp': [person.prevalentHyp for person in self.persons],
+            'diabetes': [person.diabetes for person in self.persons],
+            'totChol': [person.totChol for person in self.persons],
+            'sysBP': [person.sysBP for person in self.persons],
+            'diaBP': [person.diaBP for person in self.persons],
+            'BMI': [person.BMI for person in self.persons],
+            'heartRate': [person.heartRate for person in self.persons],
+            'glucose': [person.glucose for person in self.persons],
+            'TenYearCHD': [person.TenYearCHD for person in self.persons],
+        }
+        corr = pd.DataFrame(data).corr()
+        print(corr['TenYearCHD']['diaBP'])
         return {
-            'means': {
-                'male': mean(maleList),
-                'age': mean(ageList),
-                'education': mean(educationList),
-                'currentSmoker': mean(currentSmokerList),
-                'cigsPerDay': mean(cigsPerDayList),
-                'BPMeds': mean(BPMedsList),
-                'prevalentStroke': mean(prevalentStrokeList),
-                'prevalentHyp': mean(prevalentHypList),
-                'diabetes': mean(diabetesList),
-                'totChol': mean(totCholList),
-                'sysBP': mean(sysBPList),
-                'diaBP': mean(diaBPList),
-                'BMI': mean(BMIList),
-                'heartRate': mean(heartRateList),
-                'glucose': mean(glucoseList),
-                'TenYearCHD': mean(tenYearCHDList),
-                },
-            'sd': {
-                'male': stdev(maleList),
-                'age': stdev(ageList),
-                'education': stdev(educationList),
-                'currentSmoker': stdev(currentSmokerList),
-                'cigsPerDay': stdev(cigsPerDayList),
-                'BPMeds': stdev(BPMedsList),
-                'prevalentStroke': stdev(prevalentStrokeList),
-                'prevalentHyp': stdev(prevalentHypList),
-                'diabetes': stdev(diabetesList),
-                'totChol': stdev(totCholList),
-                'sysBP': stdev(sysBPList),
-                'diaBP': stdev(diaBPList),
-                'BMI': stdev(BMIList),
-                'heartRate': stdev(heartRateList),
-                'glucose': stdev(glucoseList),
-                'TenYearCHD': stdev(tenYearCHDList),
-                },
-            'median': {
-                'male': median(maleList),
-                'age': median(ageList),
-                'education': median(educationList),
-                'currentSmoker': median(currentSmokerList),
-                'cigsPerDay': median(cigsPerDayList),
-                'BPMeds': median(BPMedsList),
-                'prevalentStroke': median(prevalentStrokeList),
-                'prevalentHyp': median(prevalentHypList),
-                'diabetes': median(diabetesList),
-                'totChol': median(totCholList),
-                'sysBP': median(sysBPList),
-                'diaBP': median(diaBPList),
-                'BMI': median(BMIList),
-                'heartRate': median(heartRateList),
-                'glucose': median(glucoseList),
-                'TenYearCHD': median(tenYearCHDList),
-                }
+            'mean': { key: mean(val) for key, val in data.items() },
+            'min': { key: min(val) for key, val in data.items() },
+            'max': { key: max(val) for key, val in data.items() },
+            'median': { key: median(val) for key, val in data.items() },
+            'sd': { key: stdev(val) for key, val in data.items() },
+            'Q1': { key: pd.DataFrame(val).quantile(0.25)[0] for key, val in data.items() },
+            'Q3': { key: pd.DataFrame(val).quantile(0.75)[0] for key, val in data.items() },
+            'corr': { key: corr['TenYearCHD'][key] for key in data.keys() }
         }
 
     
     @classmethod
     def from_csv(cls, csvPath: str):
         csv = pd.read_csv(csvPath)
+        # Remove any rows with NaN values
+        csv = csv.dropna()
         persons = [Person( 
                             row['male'],
                             row['age'],
