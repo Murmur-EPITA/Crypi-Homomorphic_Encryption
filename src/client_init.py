40d212729d7d3ead613421a9136962b3eaf1151b
Julien Wirth <julien.wirth@epita.fr>
2023-05-08 10:14:22 +0200

    first mean test

--- src/client_init.py
+++ src/client_init.py
@@ -4,7 +4,7 @@ import csv
 import tenseal as ts
 
 # Load the CSV file into memory
-filename = "data.csv"
+filename = "./data/data.csv"
 with open(filename, "r") as csvfile:
     reader = csv.DictReader(csvfile)
     
@@ -14,38 +14,68 @@ with open(filename, "r") as csvfile:
 
     # Tupple list creation
     for row in reader:
-        data.append([float(row["male"]), \
-                    float(row["age"]), \
-                    float(row["currentSmoker"]), \
-                    float(row["cigsPerDay"]), \
-                    float(row["BPMeds"]), \
-                    float(row["prevalentStroke"]), \
-                    float(row["prevalentHyp"]), \
-                    float(row["diabetes"]), \
-                    float(row["totChol"]), \
-                    float(row["sysBP"]), \
-                    float(row["diaBP"]), \
-                    float(row["BMI"]), \
-                    float(row["heartRate"]), \
-                    float(row["glucose"]), \
-                    float(row["TenYearCHD"]), \
-                ])
+        skip = False
+        for key, value in row.items():
+            if (value == 'NA'):
+                skip = True
+        if (not skip):
+            data.append([float(row["male"]), \
+                        float(row["age"]), \
+                        float(row["currentSmoker"]), \
+                        float(row["cigsPerDay"]), \
+                        float(row["BPMeds"]), \
+                        float(row["prevalentStroke"]), \
+                        float(row["prevalentHyp"]), \
+                        float(row["diabetes"]), \
+                        float(row["totChol"]), \
+                        float(row["sysBP"]), \
+                        float(row["diaBP"]), \
+                        float(row["BMI"]), \
+                        float(row["heartRate"]), \
+                        float(row["glucose"]), \
+                        float(row["TenYearCHD"]), \
+                    ])
 
 # Initialize a context and a plaintext encoderts
 context = ts.context(ts.SCHEME_TYPE.CKKS, poly_modulus_degree=8192, coeff_mod_bit_sizes=[60, 40, 40, 60], encryption_type=ts.ENCRYPTION_TYPE.SYMMETRIC)
-encoder = ts.encoder(context)
+
+context.generate_galois_keys()
+context.global_scale = 2**40
 
 # Encrypt the data
-enc_data = []
+enc_data_global = []
+enc_data_sick = []
+enc_data_health = []
 for row in data :
-    enc_data.append(ts.ckks_vector(context, encoder.encode(row)))
+    enc_val = ts.ckks_vector(context, row)
+    # En bonne santé
+    if (row[-1] == 0):
+        enc_data_health.append(enc_val)
+    # Chance d'etre Malade
+    else :
+        enc_data_sick.append(enc_val)
+    # Toutes les données 
+    enc_data_global.append(enc_val)
 
 # Compute the mean of the age and height columns
-final_vec = ts.ckks_vector(context)
-for enc_row in enc_data:
-   final_vec += enc_row
+final_vec_global = ts.ckks_vector(context, [0] * len(data[0]))
+final_vec_sick = ts.ckks_vector(context, [0] * len(data[0]))
+final_vec_health = ts.ckks_vector(context, [0] * len(data[0]))
+for enc_row in enc_data_global:
+   final_vec_global += enc_row
+for enc_row in enc_data_sick:
+   final_vec_sick += enc_row
+for enc_row in enc_data_health:
+   final_vec_health += enc_row
 
-final_vec *= 1/len(enc_data)
+
+
+
+final_vec_global *= 1/len(enc_data_global)
+final_vec_sick *= 1/len(enc_data_sick)
+final_vec_health *= 1/len(enc_data_health)
 
 #Decrypt the results
-print("Final teub: ", encoder.decode(final_vec.decrypt()))
+print("Final All: ", final_vec_global.decrypt())
+print("Final Sick: ", final_vec_sick.decrypt())
+print("Final Health: ", final_vec_health.decrypt())
