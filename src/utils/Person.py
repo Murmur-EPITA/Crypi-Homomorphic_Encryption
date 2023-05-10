b8e03b32e07bf7f648b1d0a6a4267cefa066b9da
Angel Bochenko <angel.bochenko@epita.fr>
2023-05-10 09:10:09 +0200

    MODIFY: Person class: refactor plot operations

--- src/utils/Person.py
+++ src/utils/Person.py
@@ -33,14 +33,21 @@ class Person:
 
 class PersonList:
     def __init__(self, persons=[]):
+        '''
+            Attributes:
+                persons: list of Person instances
+                rawData: Dict of every Person attributes linked to a list of self.persons' values for this attribute
+                stats: Dict of Dict regrouping statistics for self.rawData
+        '''
         self.persons = persons
         if self.persons != []:
+            self.rawData = self.create_rawData()
             self.stats = self.compute_stats()
         else:
             self.stats = {}
-        self.tab =  self.create_tab()
+            self.rawData = {}
 
-    def create_tab(self):
+    def create_rawData(self):
         return {
             'male': [person.male for person in self.persons],
             'age': [person.age for person in self.persons],
@@ -65,24 +72,7 @@ class PersonList:
         return [person.to_list() for person in self.persons]
 
     def compute_stats(self) -> Dict[str, Dict[str, float]]:
-        data = {
-            'male': [person.male for person in self.persons],
-            'age': [person.age for person in self.persons],
-            'education': [person.education for person in self.persons],
-            'currentSmoker': [person.currentSmoker for person in self.persons],
-            'cigsPerDay': [person.cigsPerDay for person in self.persons],
-            'BPMeds': [person.BPMeds for person in self.persons],
-            'prevalentStroke': [person.prevalentStroke for person in self.persons],
-            'prevalentHyp': [person.prevalentHyp for person in self.persons],
-            'diabetes': [person.diabetes for person in self.persons],
-            'totChol': [person.totChol for person in self.persons],
-            'sysBP': [person.sysBP for person in self.persons],
-            'diaBP': [person.diaBP for person in self.persons],
-            'BMI': [person.BMI for person in self.persons],
-            'heartRate': [person.heartRate for person in self.persons],
-            'glucose': [person.glucose for person in self.persons],
-            'TenYearCHD': [person.TenYearCHD for person in self.persons],
-        }
+        data = self.rawData
         corr = pd.DataFrame(data).corr()
         return {
             'mean': { key: mean(val) for key, val in data.items() },
@@ -101,9 +91,9 @@ class PersonList:
         fig, ax = plt.subplots(figsize=(10, 7))
 
         # Création du graphique en boîtes à moustaches
-        tab = self.tab
-        my_tab = [tab['age'], tab['education'], tab['cigsPerDay'], tab['BPMeds'], tab['totChol'], tab['sysBP'], tab['diaBP'], tab['BMI'], tab['heartRate'], tab['glucose'], tab['TenYearCHD']]
-        bp = ax.boxplot(my_tab)
+        rawData = self.rawData
+        my_rawData = [rawData['age'], rawData['education'], rawData['cigsPerDay'], rawData['BPMeds'], rawData['totChol'], rawData['sysBP'], rawData['diaBP'], rawData['BMI'], rawData['heartRate'], rawData['glucose'], rawData['TenYearCHD']]
+        bp = ax.boxplot(my_rawData)
 
         # Ajout de l'axe des abscisses et des ordonnées
         ax.set_xlabel('Axe des abscisses')
@@ -140,8 +130,8 @@ class PersonList:
     
     #histogramme du taux de cholesterole en fonction de l'age on peut en faire plusieur sortes c'est juste un exemple
     def plot_cholesterol_age_histogram(self):
-        ages = self.tab['age']
-        cholesterol = self.tab['totChol']
+        ages = self.rawData['age']
+        cholesterol = self.rawData['totChol']
         plt.figure(figsize=(10, 7))
         plt.hist2d(ages, cholesterol, bins=10, range=[[0, 100], [0, 500]], cmap='YlOrRd')
         plt.colorbar(label='Nombre de personnes')
@@ -152,172 +142,70 @@ class PersonList:
         return 0
 
 
+    def _histogram(self, attr_to_compare: str, plot_title: str, ylabel_title: str):
+        malades = self.rawData['TenYearCHD']
+        attr = self.rawData[attr_to_compare]
+
+        malades_0 = [attr[int(i)] for i, malade in enumerate(malades) if malade == 0]
+        malades_1 = [attr[int(i)] for i, malade in enumerate(malades) if malade == 1]
+
+        avg_attr_0 = np.mean(malades_0)  # Moyenne cigarettes fumées pour les non-malades
+        avg_attr_1 = np.mean(malades_1)  # Moyenne cigarettes  fumées  pour les malades
+
+        attr_all = pd.Series(attr)
+        avg_attr = attr_all.mean()
+
+        plt.figure(figsize=(10, 7))
+        plt.bar([0, 1, 2], [avg_attr_0, avg_attr_1, avg_attr], color=['b', 'r', 'y'])
+        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
+        plt.title(plot_title)
+        plt.xlabel("10 year risk of coronary heart disease")
+        plt.ylabel(ylabel_title)
+        plt.show()
+        return 0
+
 
     #diagrammes à barres cigarettes par jour
     def cig_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        cigarettes = self.tab['cigsPerDay']
-
-        malades_0 = [self.tab['cigsPerDay'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-        malades_1 = [self.tab['cigsPerDay'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_cigarette_0 = np.mean(malades_0)  # Moyenne cigarettes fumées pour les non-malades
-        avg_cigarette_1 = np.mean(malades_1)  # Moyenne cigarettes  fumées  pour les malades
-
-        cig_all = pd.Series(cigarettes)
-        avg_cigarette = cig_all.mean()
-
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_cigarette_0, avg_cigarette_1, avg_cigarette], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne de cigarettes fumées par jour selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne de cigarettes fumées par jour")
-        plt.show()
-        return 0
-      
+        self._histogram('cigsPerDay', 'Moyenne de cigarettes fumées par jour selon la catégorie malade ou non',
+                       'Moyenne de cigarettes fumées par jour')
 
     #diagramme à barre moyenne taux cholesterol en fonct malades, non
     def totchol_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        cholesterol = self.tab['totChol']
-
-        malades_0 = [self.tab['totChol'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-
-        malades_1 = [self.tab['totChol'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_cholesterol_0 = np.mean(malades_0)  # Moyenne du taux de cholestérol pour les non-malades
-        avg_cholesterol_1 = np.mean(malades_1)  # Moyenne du taux de cholestérol pour les malades
-
-        
-        cholesterol_all = pd.Series(cholesterol)
-        avg_cholesterol = cholesterol_all.mean()
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_cholesterol_0, avg_cholesterol_1, avg_cholesterol], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne du taux de cholestérol selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne du taux de cholestérol")
-        plt.show()
-        return 0
-
+        self._histogram('TenYearCHD', 'Moyenne du taux de cholestérol selon la catégorie malade ou non',
+                       'Moyenne du taux de cholestérol')
 
     #diagramme à barre moyenne age en fonct malades, non
     def age_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        age = self.tab['age']
-
-        malades_0 = [self.tab['age'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-
-        malades_1 = [self.tab['age'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_age_0 = np.mean(malades_0)  # Moyenne du taux de cholestérol pour les non-malades
-        avg_age_1 = np.mean(malades_1)  # Moyenne du taux de cholestérol pour les malades
-
-        
-        age_all = pd.Series(age)
-        avg_age = age_all.mean()
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_age_0, avg_age_1, avg_age], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne des ages l selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne des ages")
-        plt.show()
-        return 0
-    
+        self._histogram('age', 'Moyenne des ages l selon la catégorie malade ou non',
+                       'Moyenne des ages')
 
     #diagramme à barre moyenne taux glucose en fonct malades, non
     def glucose_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        glucose = self.tab['glucose']
-
-        malades_0 = [self.tab['glucose'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-
-        malades_1 = [self.tab['glucose'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_glucose_0 = np.mean(malades_0)  # Moyenne du taux de cholestérol pour les non-malades
-        avg_glucose_1 = np.mean(malades_1)  # Moyenne du taux de cholestérol pour les malades
-
-        
-        glucose_all = pd.Series(glucose)
-        avg_glucose = glucose_all.mean()
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_glucose_0, avg_glucose_1, avg_glucose], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne du taux de glucose selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne du taux de glucose")
-        plt.show()
-        return 0
+        self._histogram('glucose', 'Moyenne du taux de glucose selon la catégorie malade ou non',
+                       'Moyenne du taux de glucose')
     
     #diagramme à barre moyenne bmi en fonct malades, non
     def bmi_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        bmi = self.tab['BMI']
-
-        malades_0 = [self.tab['BMI'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-
-        malades_1 = [self.tab['BMI'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_bmi_0 = np.mean(malades_0)  # Moyenne du taux de cholestérol pour les non-malades
-        avg_bmi_1 = np.mean(malades_1)  # Moyenne du taux de cholestérol pour les malades
-
-        
-        bmi_all = pd.Series(bmi)
-        avg_bmi= bmi_all.mean()
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_bmi_0, avg_bmi_1, avg_bmi], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne du taux de bmi selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne du taux de bmi")
-        plt.show()
-        return 0
+        self._histogram('BMI', 'Moyenne du taux de bmi selon la catégorie malade ou non',
+                       'Moyenne du taux de bmi')
 
     #diagramme à barre moyenne heartRate en fonct malades, non
     def heartRate_malade_histogram(self):
-        malades = self.tab['TenYearCHD']
-        heartRate = self.tab['heartRate']
-
-        malades_0 = [self.tab['heartRate'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 0]
-
-        malades_1 = [self.tab['heartRate'][int(i)] for i, malade in enumerate(self.tab['TenYearCHD']) if malade == 1]
-
-        avg_heartRate_0 = np.mean(malades_0)  # Moyenne du taux de cholestérol pour les non-malades
-        avg_heartRate_1 = np.mean(malades_1)  # Moyenne du taux de cholestérol pour les malades
-
-        
-        heartRate_all = pd.Series(heartRate)
-        avg_heartRate = heartRate_all.mean()
-
-        plt.figure(figsize=(10, 7))
-        plt.bar([0, 1, 2], [avg_heartRate_0, avg_heartRate_1, avg_heartRate], color=['b', 'r', 'y'])
-        plt.xticks([0, 1, 2], ['Non-malades', 'Malades', 'everybody'])
-        plt.title("Moyenne du taux de heartRate selon la catégorie malade ou non")
-        plt.xlabel("10 year risk of coronary heart disease")
-        plt.ylabel("Moyenne du taux de heartRate")
-        plt.show()
-        return 0
-
-
+        self._histogram('heartRate', 'Moyenne du taux de heartRate selon la catégorie malade ou non',
+                       'Moyenne du taux de heartRate')
 
     #histogramme avec toutes les colonnes
     def stats_hist(self):
 
-        tab = self.tab
-        my_tab = [tab['age'], tab['education'], tab['cigsPerDay'], tab['BPMeds'], tab['totChol'], tab['sysBP'], tab['diaBP'], tab['BMI'], tab['heartRate'], tab['glucose']]
+        rawData = self.rawData
+        my_rawData = [rawData['age'], rawData['education'], rawData['cigsPerDay'], rawData['BPMeds'], rawData['totChol'], rawData['sysBP'], rawData['diaBP'], rawData['BMI'], rawData['heartRate'], rawData['glucose']]
         
         plt.figure(figsize=(12, 10))
 
         colonnes = ["age", "education", "cigsPerDay", "totChol", "sysBP", "diaBP", "BMI", "heartRate", "glucose"] # Liste de noms correspondant à chaque histogramme
 
-        for i, data in enumerate(my_tab):
+        for i, data in enumerate(my_rawData):
             color_map = cm.get_cmap('tab10')  # Crée une nouvelle carte de couleurs pour chaque histogramme
             color = color_map(i % 10)  # Utilise un index différent pour chaque histogramme
             plt.hist(data, bins=10, range=(0, 500), align="mid", rwidth=0.9, color=color, edgecolor="black", label=f'Boîte {i+1}')
